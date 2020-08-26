1dd5c2d15c396cf0532ed4ca26020ab0dd189061
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-08-26 15:43:53 -0400

    Adding navbar styles

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,14 +1,33 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
 
-const Navbar = () => {
+const Navbar = ({ navigation }) => {
   return (
-    <View>
-      <Text>Navbar</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.tinyLogo}
+        source={require('../assets/img/pokeIcon.png')}
+      />
+      <TouchableOpacity onPress={() => navigation.push('CatchThem')}>
+        <Text>Buscar</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default Navbar
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 2,
+    backgroundColor: 'rgba(4, 4, 4, 0.2)',
+  },
+  tinyLogo: {
+    width: 30,
+    height: 30,
+  },
+})
