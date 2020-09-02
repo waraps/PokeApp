fbf4a548543a4a1d9c3543f54c35b441b673929e
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-09-02 08:02:57 -0400

    Changing file

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
 import React, {useState, useEffect} from 'react'
-// import * as Font from 'expo-font';
+import * as Font from 'expo-font';
 import Router from './app/routes/Router'
 
-// import ActivityIndicatorApp from './app/components/ActivityIndicatorApp'
+import ActivityIndicatorApp from './app/components/ActivityIndicatorApp'
 
 export default function App() {
 
-  // const [loaded, setLoaded] = useState(true)
-  // useEffect(() => {
-  //   loadFonts()
-  // }, [])
+  const [loaded, setLoaded] = useState(true)
+  useEffect(() => {
+    loadFonts()
+  }, [])
 
-  // const loadFonts = async () => {
-  //   await Font.loadAsync({
-  //     'ValeriaRound-Regular': require('./app/assets/font/VarelaRound-Regular.ttf')
-  //   })
-  //   setLoaded(false)
-  // }
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      'ValeriaRound-Regular': require('./app/assets/font/VarelaRound-Regular.ttf')
+    })
+    setLoaded(false)
+  }
 
-  // if (!loaded) {
+  if (!loaded) {
     return <Router />
-  // }
-  // return <ActivityIndicatorApp />
+  }
+  return <ActivityIndicatorApp />
   
 }
