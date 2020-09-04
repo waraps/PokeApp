2a5eb0c0a46d42594e564ea1c16eb92b9d2dd806
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-09-04 16:38:32 -0400

    Showing pokemons catched

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,21 +1,44 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, Text } from 'react-native'
 
 // Expo imports
 import Constants from 'expo-constants'
 
 // Components
 import Navbar from '../components/Navbar'
+import PokeCard from '../components/PokeCard'
 
 // Assests
 import { COLORS } from '../utils/colors'
 
+// LocalStorage
+import { localStorgae } from '../storage/localStorage'
+
 export default function Home({ navigation }) {
+  const [pokemons, setPokemons] = useState([])
+  useEffect(() => {
+    getPokemons()
+  }, [])
+  const getPokemons = async () => {
+    try {
+      const pokemonsList = await localStorgae.getPokemon()
+      if (pokemonsList) {
+        setPokemons(pokemonsList)
+      }
+    } catch (error) {
+      console.log(error + 'error al cargar pokemones')
+    }
+  }
   return (
     <View style={styles.container}>
       <Navbar style={styles.navbar} navigation={navigation} />
-      <StatusBar style='light' backgroundColor='#9A0000'/>
+      <View style={styles.pokeList}>
+      {pokemons.map(pokemon => {
+        return <PokeCard key={pokemon.name} pokemon={pokemon} />
+      })}
+      </View>
+      <StatusBar style='light' backgroundColor='#9A0000' />
     </View>
   )
 }
@@ -24,9 +47,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.white,
-    paddingTop: Constants.statusBarHeight,
+    paddingTop: Constants.statusBarHeight
   },
   navbar: {
-    marginTop: 30,
+    marginTop: 30
   },
+  pokeList: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  }
 })
