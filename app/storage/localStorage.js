9cfd7a70b2ae420c45c397359af42402958205a5
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-09-04 16:36:28 -0400

    Adding local storage

--- app/storage/localStorage.js
+++ app/storage/localStorage.js
@@ -1,5 +1,18 @@
 import AsyncStorage from "@react-native-community/async-storage";
 
 export const localStorgae = {
-  
+  async setPokemon(pokemon) {
+    try {
+      await AsyncStorage.setItem("pokemon", JSON.stringify(pokemon));
+    } catch (error) {
+      console.log(error + ", no se pudo guardar pokemon");
+    }
+  },
+  async getPokemon() {
+    try {
+      return JSON.parse(await AsyncStorage.getItem("pokemon"));
+    } catch (error) {
+      console.log(error + ", no se pudo obtener pokemon");
+    }
+  },
 }
