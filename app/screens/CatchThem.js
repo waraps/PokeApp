df8f0e8703797cc2530d8c89f664636e8d613fec
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-09-04 16:37:47 -0400

    Adding catch pokemon feature

--- app/screens/CatchThem.js
+++ app/screens/CatchThem.js
@@ -1,90 +1,113 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from "react-native";
+import { StatusBar } from 'expo-status-bar'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 
 // Expo imports
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign } from '@expo/vector-icons'
 
 // Components
-import ActivityIndicatorApp from "../components/ActivityIndicatorApp";
-import PokeDisplay from "../components/PokeDisplay";
+import ActivityIndicatorApp from '../components/ActivityIndicatorApp'
+import PokeDisplay from '../components/PokeDisplay'
 
 // API
-import { catchPokemon } from "../api/index";
+import { catchPokemon } from '../api/index'
+
+// LocalStorage
+import { localStorgae } from '../storage/localStorage'
 
 // Assests
-import { COLORS } from "../utils/colors";
+import { COLORS } from '../utils/colors'
 
 export default function CatchThem({ navigation }) {
-  const [search, setSearch] = useState('1');
-  const [pokemon, setPokemon] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [search, setSearch] = useState('1')
+  const [pokemon, setPokemon] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
   useEffect(() => {
-    getPokemon();
-  }, [search]);
+    getPokemon()
+  }, [search])
 
   const getPokemon = async () => {
     try {
       setIsLoading(true)
-      const response = await catchPokemon.catchIt(search.length === 0 ? '1' : search);
-      const pokeResponse = await response.json();
-      setPokemon(pokeResponse);
-      setIsLoading(false);
+      const response = await catchPokemon.catchIt(search.length === 0 ? '1' : search)
+      const pokeResponse = await response.json()
+      setPokemon(pokeResponse)
+      setIsLoading(false)
     } catch (error) {
-      console.log(error + "Error al capturar pokemon");
-      setIsLoading(false);
+      console.log(error + 'Error al capturar pokemon')
+      setIsLoading(false)
     }
-  };
+  }
+
+  const catchingIt = async () => {
+    try {
+      setIsLoading(true)
+      let pokemons = await localStorgae.getPokemon()
+      if (pokemons) {
+        pokemons.push(pokemon)
+        await localStorgae.setPokemon(pokemons)
+        console.log('guardado')
+      } else {
+        let list = []
+        list.push(pokemon)
+        await localStorgae.setPokemon(list)
+      }
+      setIsLoading(false)
+    } catch (error) {
+      console.log(erro + 'Error al capturar pokemon')
+      setIsLoading(false)
+    }
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.search}>
         <TextInput
           style={styles.searchInput}
-          placeholder="Search"
+          placeholder='Search'
           placeholderTextColor={COLORS.navyBlue}
-          underlineColorAndroid="transparent"
+          underlineColorAndroid='transparent'
           maxLength={24}
-          onChangeText={(text) => setSearch(text)}
+          onChangeText={text => setSearch(text)}
           value={search}
         />
-        <AntDesign name="search1" size={28} color={COLORS.navyBlue} />
+        <AntDesign name='search1' size={28} color={COLORS.navyBlue} />
       </View>
       <PokeDisplay pokemon={pokemon} isLoading={isLoading} />
-      <TouchableOpacity style={styles.catchButton}>
+      <TouchableOpacity style={styles.catchButton} onPress={catchingIt}>
         <Text style={styles.catchText}>Catch it</Text>
       </TouchableOpacity>
-      <StatusBar style="light" backgroundColor={COLORS.navyBlue} />
+      <StatusBar style='light' backgroundColor={COLORS.navyBlue} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.darkRed,
-    alignItems: "center",
-    justifyContent: "space-around",
+    alignItems: 'center',
+    justifyContent: 'space-around'
   },
   search: {
-    width: "95%",
+    width: '95%',
     paddingHorizontal: 20,
     paddingVertical: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     borderRadius: 60,
-    backgroundColor: COLORS.white,
+    backgroundColor: COLORS.white
   },
   searchInput: {
-    width: "95%",
-    fontSize: 15,
+    width: '95%',
+    fontSize: 15
   },
   catchButton: {
     width: '80%',
     height: 50,
     padding: 20,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: COLORS.lightBlue,
     borderWidth: 1,
     borderRadius: 30,
@@ -93,6 +116,6 @@ const styles = StyleSheet.create({
   catchText: {
     color: COLORS.white,
     fontSize: 20,
-    fontWeight: "bold",
+    fontWeight: 'bold'
   }
-});
+})
