3aefd05104880148a4b8cc805236fa15b9c40827
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-09-07 10:16:16 -0400

    Formatting text an adding styles

--- app/components/PokeCard.js
+++ app/components/PokeCard.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react'
 import { StyleSheet, TouchableOpacity, View, Text, Image} from 'react-native'
 
+import { COLORS } from '../utils/colors'
+
 export default class PokeCard extends Component {
   render() {
     const { pokemon } = this.props
     return (
-      <TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
         <Image style={styles.tinyLogo} source={{ uri: pokemon.sprites.front_default }}/>
-        <Text style={styles.text}> {pokemon.name} </Text>
-        <Text style={styles.text}> {pokemon.weight} </Text>
-        <Text style={styles.text}> {pokemon.height} </Text>
+        <Text style={[styles.text, {textTransform: 'capitalize', fontWeight: 'bold'}]}> {pokemon.name} </Text>
+        <Text style={{fontSize: 12, textAlign: "center",}}> W: {pokemon.weight * 0.1} Kg</Text>
+        <Text style={{fontSize: 12, textAlign: "center",}}> H: {(pokemon.height * 0.1).toFixed(1)} m</Text>
       </TouchableOpacity>
     )
   }
 }
 
 const styles = StyleSheet.create({
+  button: {
+    borderWidth: .2,
+    borderColor: COLORS.lightBlue,
+    borderRadius: 15,
+  },
   tinyLogo: {
     width: 100,
     height: 100,
