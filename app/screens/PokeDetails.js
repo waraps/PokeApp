16596512c025a557dc8bd4fd9217db8ffc666e5a
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-09-09 10:48:17 -0400

    Adding scroll to details

--- app/screens/PokeDetails.js
+++ app/screens/PokeDetails.js
@@ -1,48 +1,95 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+
+// Expo imports
+import Constants from "expo-constants";
 
 // Assests
-import { COLORS } from '../utils/colors'
+import { COLORS } from "../utils/colors";
 
 const PokeDetails = ({ route }) => {
-  const { pokemon } = route.params
+  const { pokemon } = route.params;
   return (
     <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image style={styles.tinyLogo} source={{ uri: pokemon.sprites.front_default }} />
-        <Image style={styles.tinyLogo} source={{ uri: pokemon.sprites.back_default }} />
+      <View style={styles.pokeContainer}>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.tinyLogo}
+            source={{ uri: pokemon.sprites.front_default }}
+          />
+          <Image
+            style={styles.tinyLogo}
+            source={{ uri: pokemon.sprites.back_default }}
+          />
+        </View>
+        <View style={styles.pokeNameContainer}>
+          <Text style={styles.pokeName}>{pokemon.name}</Text>
+        </View>
       </View>
-      <View style={styles.pokeNameContainer}>
-        <Text style={styles.pokeName}>{pokemon.name}</Text>
-      </View>
-      <StatusBar style='light' backgroundColor={COLORS.lightBlue} />
+      <ScrollView >
+        <Text> details test first</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test</Text>
+        <Text> details test last</Text>
+      </ScrollView>
+      <StatusBar style="light" backgroundColor={COLORS.lightBlue} />
     </View>
-  )
-}
+  );
+};
 
-export default PokeDetails
+export default PokeDetails;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    marginTop: Constants.statusBarHeight,
+  },
+  pokeContainer: {
+    backgroundColor: COLORS.yellow,
   },
   logoContainer: {
-    flexDirection: 'row'
+    flexDirection: "row",
+    justifyContent: "center",
+    padding: 5,
   },
   tinyLogo: {
     width: 150,
-    height: 150
+    height: 150,
   },
   pokeNameContainer: {
-    justifyContent: 'center',
-    alignItems: 'center'
+    marginTop: -20,
+    alignItems: "center",
   },
   pokeName: {
     fontSize: 35,
-    textTransform: 'uppercase',
+    textTransform: "uppercase",
     fontFamily: "ValeriaRound-Regular",
+    color: COLORS.white,
+    textShadowColor: COLORS.black,
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
+  },
+  scrollDetails: {
+    padding: 5,
   }
-})
+});
