bc28cbcd7f135922e869e90f39fea1e822c5960c
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-09-16 11:52:26 -0400

    Adding transitions to navigation

--- app/routes/Router.js
+++ app/routes/Router.js
@@ -1,18 +1,52 @@
 import * as React from "react";
+import { Easing } from "react-native";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, transitionSpec, CardStyleInterpolators, TransitionPresets } from "@react-navigation/stack";
 
 // Screens
 import Home from "../screens/Home";
 import CatchThem from "../screens/CatchThem";
 import PokeDetails from "../screens/PokeDetails";
 
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 400,
+    mass: 3,
+    overshootClamping: false,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
+
+const configClose = {
+  animation: 'timing',
+  config: {
+    duration: 400,
+    easing: Easing.linear
+  },
+};
+
 const Stack = createStackNavigator();
 export default function Router() {
   return (
     <NavigationContainer>
-      <Stack.Navigator InitialRouteName={Home}>
+      <Stack.Navigator InitialRouteName={Home}
+        screenOptions={{
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+          // ...TransitionPresets.SlideFromRightIOS,
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          transitionSpec: {
+            open: config,
+            close: configClose,
+          }
+        }}
+        animation="fade"
+
+      >
         <Stack.Screen
           name="Home"
           component={Home}
