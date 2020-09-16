38392fc18b71da9d7372b1afb9e1c393ed471f1b
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-09-15 21:06:55 -0400

    Adding free pokemon feature

--- app/components/PokeCard.js
+++ app/components/PokeCard.js
@@ -1,29 +1,68 @@
 import React, { Component } from "react";
-import { StyleSheet, TouchableOpacity, Text, Image } from "react-native";
+import { StyleSheet, View, TouchableOpacity, Text, Image } from "react-native";
+import { StackActions } from "@react-navigation/native";
 
 // Utils
 import { COLORS } from "../utils/colors";
 
+//Components
+import Confirm from "./Confirm";
+
+// LocalStorage
+import { localStorgae } from "../storage/localStorage";
+
 export default class PokeCard extends Component {
+  deletePokemon = async () => {
+    try {
+      const { index, navigation } = this.props;
+      let pokemonsList = await localStorgae.getPokemon();
+      pokemonsList.splice(index, index);
+      await localStorgae.setPokemon(pokemonsList);
+      navigation.dispatch(StackActions.replace("Home"));
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  confirmDelete = () => {
+    try {
+      const data = {
+        title: "Free Pokemon",
+        message: "Are you sure that want to free this pokemon?",
+        action: () => this.deletePokemon(),
+      };
+      Confirm(data);
+    } catch (error) {
+      alert(error);
+      console.log(error);
+    }
+  };
+
   render() {
     const { pokemon, navigation } = this.props;
     return (
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.push("PokeDetails", { pokemon })}
-      >
-        <Image
-          style={styles.tinyLogo}
-          source={{ uri: pokemon.sprites.front_default }}
-        />
-        <Text style={styles.textName}> {pokemon.name} </Text>
-        <Text style={styles.textMeasure}>
-          W: {(pokemon.weight * 0.1).toFixed(2)} Kg
-        </Text>
-        <Text style={styles.textMeasure}>
-          H: {(pokemon.height * 0.1).toFixed(2)} m
-        </Text>
-      </TouchableOpacity>
+      <View style={styles.button}>
+        <TouchableOpacity
+          onPress={() => navigation.push("PokeDetails", { pokemon })}
+        >
+          <Image
+            style={styles.tinyLogo}
+            source={{ uri: pokemon.sprites.front_default }}
+          />
+          <Text style={styles.textName}> {pokemon.name} </Text>
+          <Text style={styles.textMeasure}>
+            W: {(pokemon.weight * 0.1).toFixed(2)} Kg
+          </Text>
+          <Text style={styles.textMeasure}>
+            H: {(pokemon.height * 0.1).toFixed(2)} m
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.confirmDelete}>
+          <View style={styles.deleteContainer}>
+            <Text style={styles.deleteText}>Free Pokemon</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
@@ -31,9 +70,7 @@ export default class PokeCard extends Component {
 const styles = StyleSheet.create({
   button: {
     marginVertical: 3,
-    paddingBottom: 3,
     marginHorizontal: 4,
-    paddingHorizontal: 2,
     borderRadius: 8,
     backgroundColor: COLORS.white,
     elevation: 9,
@@ -50,5 +87,18 @@ const styles = StyleSheet.create({
   textMeasure: {
     fontSize: 12,
     textAlign: "center",
+    fontFamily: "ValeriaRound-Regular",
+  },
+  deleteContainer: {
+    backgroundColor: "red",
+    marginTop: 2,
+    width: "100%",
+    borderBottomRightRadius: 8,
+    borderBottomLeftRadius: 8,
+  },
+  deleteText: {
+    textAlign: "center",
+    color: "#fff",
+    fontFamily: "ValeriaRound-Regular",
   },
 });
