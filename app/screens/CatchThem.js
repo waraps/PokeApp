0a4afa888eed50f8664f4bc844ae9165b7a46f2c
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-09-18 15:31:42 -0400

    Validating memory size for pokemons storage

--- app/screens/CatchThem.js
+++ app/screens/CatchThem.js
@@ -1,49 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  ScrollView,
-} from "react-native";
-import { CommonActions } from "@react-navigation/native";
+import { StatusBar } from 'expo-status-bar'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native'
+import { CommonActions } from '@react-navigation/native'
+import { sizeof } from 'sizeof'
 
 // Expo imports
-import Constants from "expo-constants";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign } from '@expo/vector-icons'
 
 // Components
-import PokeDisplay from "../components/PokeDisplay";
+import PokeDisplay from '../components/PokeDisplay'
 
 // API
-import { catchPokemon } from "../api/index";
+import { catchPokemon } from '../api/index'
 
 // LocalStorage
-import { localStorgae } from "../storage/localStorage";
+import { localStorgae } from '../storage/localStorage'
 
 // Utils
-import { COLORS } from "../utils/colors";
+import { COLORS } from '../utils/colors'
 
-export default function CatchThem({ navigation }) {
-  const [search, setSearch] = useState("1");
-  const [pokemon, setPokemon] = useState(null);
-  const [myPokemons, setMyPokemons] = useState(undefined);
-  const [isLoading, setIsLoading] = useState(false);
+export default function CatchThem({ route, navigation }) {
+  const [search, setSearch] = useState('1')
+  const [pokemon, setPokemon] = useState(null)
+  const [myPokemons, setMyPokemons] = useState(undefined)
+  const [isLoading, setIsLoading] = useState(false)
+
   useEffect(() => {
-    getStoragedPokemon();
-    getPokemon();
-  }, [search]);
+    getStoragedPokemon()
+    getPokemon()
+  }, [search])
 
   const getPokemon = async () => {
     try {
-      setIsLoading(true);
-      const response = await catchPokemon.catchIt(
-        search.length === 0 ? "1" : search
-      );
-      const pokeResponse = await response.json();
+      setIsLoading(true)
+      const response = await catchPokemon.catchIt(search.length === 0 ? '1' : search)
+      const pokeResponse = await response.json()
       let pokeFormat = {
+        id: pokeResponse.id,
         abilities: pokeResponse.abilities,
         base_experience: pokeResponse.base_experience,
         height: pokeResponse.height,
@@ -52,68 +45,89 @@ export default function CatchThem({ navigation }) {
         sprites: pokeResponse.sprites,
         stats: pokeResponse.stats,
         types: pokeResponse.types,
-        weight: pokeResponse.weight,
-      };
-      setPokemon(pokeFormat);
-      setIsLoading(false);
+        weight: pokeResponse.weight
+      }
+      setPokemon(pokeFormat)
+      setIsLoading(false)
     } catch (error) {
-      console.log(error + "error to catch pokemon");
-      setIsLoading(false);
+      console.log(error + 'error to catch pokemon')
+      setIsLoading(false)
     }
-  };
+  }
 
   const getStoragedPokemon = async () => {
     try {
-      setMyPokemons(await localStorgae.getPokemon());
+      setMyPokemons(await localStorgae.getPokemon())
     } catch (error) {
-      console.log(error + "error to get pokemons");
+      console.log(error + 'error to get pokemons')
     }
-  };
+  }
+
+  const existPokemon = () => {
+    const check = myPokemons.find(poke => {
+      return pokemon.name === poke.name
+    })
+    return check
+  }
+
+  const isMemoryfull = () => {
+    const { memorySize } = route.params
+    const pokeSize = sizeof(pokemon)
+    const totalSize = memorySize + pokeSize
+    if (totalSize < 6000000) {
+      return true
+    }
+    return false
+  }
+
+  const savePokemon = async list => {
+    setMyPokemons(list)
+    await localStorgae.setPokemon(list)
+    setIsLoading(false)
+    newNavigate()
+  }
 
   const newNavigate = () => {
     navigation.dispatch(
       CommonActions.reset({
         index: 1,
-        routes: [{ name: "Home" }],
+        routes: [{ name: 'Home' }]
       })
-    );
-  };
+    )
+  }
 
   const catchingIt = async () => {
     try {
-      setIsLoading(true);
-      let list = [];
-      if (myPokemons) {
-        const check = myPokemons.find((poke) => {
-          return pokemon.name === poke.name;
-        });
-        if (!check) {
-          list = myPokemons;
-          if (pokemon != null) {
-            list.push(pokemon);
-            setMyPokemons(list);
-            await localStorgae.setPokemon(list);
+      setIsLoading(true)
+      if (pokemon != null) {
+        let list = []
+        if (myPokemons) {
+          if (!existPokemon(myPokemons)) {
+            if (isMemoryfull()) {
+              list = myPokemons
+              list.push(pokemon)
+              savePokemon(list)
+            } else {
+              setIsLoading(false)
+              alert('Memory full, you should free a pokemon')
+            }
+          } else {
+            setIsLoading(false)
+            alert('This pokemon already was catched')
           }
-          setIsLoading(false);
-          newNavigate();
         } else {
-          setIsLoading(false);
-          alert("This pokemon already was catched");
+          list.push(pokemon)
+          await localStorgae.setPokemon(list)
+          setIsLoading(false)
+          newNavigate()
         }
-      } else {
-        if (pokemon != null) {
-          list.push(pokemon);
-          await localStorgae.setPokemon(list);
-        }
-        setIsLoading(false);
-        newNavigate();
       }
     } catch (error) {
-      alert("Error to catch pokemon");
-      console.log(erro + "Error to catch pokemon");
-      setIsLoading(false);
+      alert('Error to catch pokemon')
+      console.log(error + 'Error to catch pokemon')
+      setIsLoading(false)
     }
-  };
+  }
 
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
@@ -121,67 +135,63 @@ export default function CatchThem({ navigation }) {
         <View style={styles.search}>
           <TextInput
             style={styles.searchInput}
-            placeholder="name or id (Ex. bulbasaur or 1)"
+            placeholder='name or id (Ex. bulbasaur or 1)'
             placeholderTextColor={COLORS.navyBlue}
-            underlineColorAndroid="transparent"
+            underlineColorAndroid='transparent'
             maxLength={24}
-            onChangeText={(text) => setSearch(text)}
+            onChangeText={text => setSearch(text)}
             value={search}
-            autoCapitalize="none"
+            autoCapitalize='none'
           />
-          <AntDesign name="search1" size={28} color={COLORS.navyBlue} />
+          <AntDesign name='search1' size={28} color={COLORS.navyBlue} />
         </View>
         <PokeDisplay pokemon={pokemon} isLoading={isLoading} />
-        <TouchableOpacity
-          style={styles.catchButton}
-          onPress={catchingIt}
-          disabled={isLoading}
-        >
+        <TouchableOpacity style={styles.catchButton} onPress={catchingIt} disabled={isLoading}>
           <Text style={styles.catchText}>CATCH !</Text>
         </TouchableOpacity>
-        <StatusBar style="light" backgroundColor={COLORS.navyBlue} />
+        <StatusBar style='light' backgroundColor={COLORS.navyBlue} />
       </View>
     </ScrollView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.darkRed,
-    alignItems: "center",
-    justifyContent: "space-around",
+    alignItems: 'center',
+    justifyContent: 'space-around'
   },
   search: {
-    width: "95%",
+    width: '95%',
     paddingHorizontal: 20,
     paddingVertical: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     borderRadius: 60,
     backgroundColor: COLORS.white,
-    marginVertical: 30,
+    marginVertical: 30
   },
   searchInput: {
-    width: "95%",
+    width: '95%',
     fontSize: 15,
-    fontFamily: "ValeriaRound-Regular",
+    fontFamily: 'ValeriaRound-Regular'
   },
   catchButton: {
-    width: "80%",
+    width: '80%',
     height: 50,
     padding: 20,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: COLORS.lightBlue,
     borderWidth: 1,
     borderRadius: 30,
     borderColor: COLORS.yellow,
-    marginVertical: 30,
+    marginVertical: 30
   },
   catchText: {
     color: COLORS.white,
     fontSize: 20,
-    fontFamily: "ValeriaRound-Regular",
-  },
-});
+    fontFamily: 'ValeriaRound-Regular'
+  }
+})
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,47 +1,55 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Text, ScrollView } from "react-native";
+import { StatusBar } from 'expo-status-bar'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, Text, ScrollView } from 'react-native'
+import { sizeof } from 'sizeof'
 
 // Expo imports
-import Constants from "expo-constants";
+import Constants from 'expo-constants'
 
 // Components
-import Navbar from "../components/Navbar";
-import PokeCard from "../components/PokeCard";
+import IsLoading from '../components/IsLoading'
+import Navbar from '../components/Navbar'
+import PokeCard from '../components/PokeCard'
 
 // Utils
-import { COLORS } from "../utils/colors";
+import { COLORS } from '../utils/colors'
 
 // LocalStorage
-import { localStorgae } from "../storage/localStorage";
+import { localStorgae } from '../storage/localStorage'
 
 export default function Home({ navigation }) {
-  const [pokemons, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
+  const [pokemons, setPokemons] = useState([])
+  const [memorySize, setMemorySize] = useState(0)
   useEffect(() => {
-    getPokemons();
-  }, []);
+    getPokemons()
+  }, [])
   const getPokemons = async () => {
     try {
-      const pokemonsList = await localStorgae.getPokemon();
+      const pokemonsList = await localStorgae.getPokemon()
       if (pokemonsList) {
-        setPokemons(pokemonsList);
+        setPokemons(pokemonsList)
+        setMemorySize(sizeof(pokemonsList))
+        setIsLoading(false)
       }
     } catch (error) {
-      alert("Error to trying show pokemons");
-      console.log(error + "error to trying show pokemons");
+      setIsLoading(false)
+      alert('Error to trying show pokemons')
+      console.log(error + 'error to trying show pokemons')
     }
-  };
+  }
+
+  if (isLoading) {
+    return <IsLoading />
+  }
+
   return (
     <View style={styles.container}>
-      <Navbar style={styles.navbar} navigation={navigation} />
+      <Navbar style={styles.navbar} navigation={navigation} memorySize={memorySize} />
       <View style={styles.pokeCount}>
-        <Text style={styles.pokeCountText}>
-          Total Pokemons catched: {pokemons.length}
-        </Text>
+        <Text style={styles.pokeCountText}>Total Pokemons catched: {pokemons.length}</Text>
       </View>
-      <ScrollView
-        contentContainerStyle={{ flexGrow: 1, backgroundColor: COLORS.white }}
-      >
+      <ScrollView contentContainerStyle={{ flexGrow: 1, backgroundColor: COLORS.white }}>
         <View style={styles.pokeList}>
           {pokemons.map((pokemon, index) => {
             return (
@@ -52,38 +60,38 @@ export default function Home({ navigation }) {
                 pokemon={pokemon}
                 navigation={navigation}
               />
-            );
+            )
           })}
         </View>
       </ScrollView>
-      <StatusBar style="light" backgroundColor="#9A0000" />
+      <StatusBar style='light' backgroundColor='#9A0000' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.white,
-    paddingTop: Constants.statusBarHeight - 6,
+    paddingTop: Constants.statusBarHeight - 6
   },
   navbar: {
-    marginTop: 30,
+    marginTop: 30
   },
   pokeCount: {
     backgroundColor: COLORS.black,
-    padding: 5,
+    padding: 5
   },
   pokeCountText: {
-    textAlign: "center",
-    fontFamily: "ValeriaRound-Regular",
+    textAlign: 'center',
+    fontFamily: 'ValeriaRound-Regular',
     fontSize: 16,
-    color: COLORS.white,
+    color: COLORS.white
   },
   pokeList: {
     padding: 5,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    backgroundColor: COLORS.white,
-  },
-});
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    backgroundColor: COLORS.white
+  }
+})
