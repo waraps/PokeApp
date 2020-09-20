9c60f8c56ef390b222fa153b5cf08159d96fa427
Manuel Meneses <manuelmenesesg@hotmail.com>
2020-09-20 10:02:18 -0400

    Formatting code

--- app/components/IsLoading.js
+++ app/components/IsLoading.js
@@ -1,36 +1,36 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 // Utils
-import { COLORS } from '../utils/colors'
+import { COLORS } from "../utils/colors";
 
-import ActivityIndicatorApp from '../components/ActivityIndicatorApp'
+import ActivityIndicatorApp from "../components/ActivityIndicatorApp";
 
 const IsLoading = () => {
   return (
     <View style={styles.isLoadingContainer}>
       <ActivityIndicatorApp />
       <Text style={styles.isLoadingText}>Loading Pokemon List ...</Text>
-      <StatusBar style='light' backgroundColor={COLORS.navyBlue} />
+      <StatusBar style="light" backgroundColor={COLORS.navyBlue} />
     </View>
-  )
-}
+  );
+};
 
-export default IsLoading
+export default IsLoading;
 
 const styles = StyleSheet.create({
   isLoadingContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: COLORS.red
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.red,
   },
   isLoadingText: {
-    textAlign: 'center',
+    textAlign: "center",
     color: COLORS.white,
     fontSize: 20,
-    fontWeight: 'bold',
-    padding: 10
-  }
-})
+    fontWeight: "bold",
+    padding: 10,
+  },
+});
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,56 +1,62 @@
-import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 
 // Utils
-import { COLORS } from '../utils/colors'
+import { COLORS } from "../utils/colors";
 
 const Navbar = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Image style={styles.tinyLogo} source={require('../assets/img/pokeIcon.png')} />
+      <Image
+        style={styles.tinyLogo}
+        source={require("../assets/img/pokeIcon.png")}
+      />
       <TouchableOpacity
         style={styles.search}
-        onPress={() => navigation.push('CatchThem')}
+        onPress={() => navigation.push("CatchThem")}
       >
         <Text style={styles.searchText}>catch'em</Text>
-        <Image style={styles.dittoTinyLogo} source={require('../assets/img/ditto.png')} />
+        <Image
+          style={styles.dittoTinyLogo}
+          source={require("../assets/img/ditto.png")}
+        />
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingHorizontal: 10,
     paddingTop: 10,
     backgroundColor: COLORS.darkRed,
     borderWidth: 0.8,
-    borderColor: 'rgba(154, 0, 0, 0.8)'
+    borderColor: "rgba(154, 0, 0, 0.8)",
   },
   tinyLogo: {
     width: 40,
-    height: 40
+    height: 40,
   },
   search: {
-    flexDirection: 'column-reverse',
-    justifyContent: 'center',
-    alignItems: 'center'
+    flexDirection: "column-reverse",
+    justifyContent: "center",
+    alignItems: "center",
   },
   searchText: {
     color: COLORS.yellow,
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 12,
-    fontFamily: 'ValeriaRound-Regular'
+    fontFamily: "ValeriaRound-Regular",
   },
   dittoTinyLogo: {
     marginTop: 5,
     width: 35,
-    height: 30
-  }
-})
+    height: 30,
+  },
+});
